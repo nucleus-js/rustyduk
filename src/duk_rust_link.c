f6ca77f7cb9e501871b90b3fcabb1822c465804e
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2016-07-20 02:57:09 -0700

    Working, basic Rust <--> duktape linkage (#1)
    Currently runs JS files: target/debug/rusty-duktape test.js
    
    Clone with --depth=1 for submodules

---
+++ src/duk_rust_link.c
@@ -0,0 +1,13 @@
+
+// Silly duktape header linkings because Rust doesn't do it (yet?),
+// and `bindgen` doesn't work good enough yet.
+
+#include "../deps/duktape-releases/src/duktape.h"
+
+duk_context *rust_duk_create_heap_default(void) {
+  return duk_create_heap_default();
+}
+
+duk_int_t rust_duk_peval_file(duk_context *ctx, const char *path) {
+  return duk_peval_file(ctx, path);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
+// stdlib imports
+use std::{env, fs};
+use std::ffi::CString;
+
+// module imports
+extern crate libc;
+use libc::{c_char, c_int};
+
 extern crate getopts;
 use getopts::Options;
-use std::env;
+//
+
+// Intentionally has the same name as the C struct
+#[allow(non_camel_case_types)]
+enum duk_context {}
+
+extern {
+    fn rust_duk_create_heap_default() -> *mut duk_context;
+    fn rust_duk_peval_file(ctx: *mut duk_context, path: *const c_char) -> c_int;
+    fn duk_destroy_heap(ctx: *mut duk_context);
+}
+
 
 fn print_usage(program: &str, opts: Options) {
     let brief = format!("Usage: {} FILE [options]", program);
@@ -8,25 +27,44 @@ fn print_usage(program: &str, opts: Options) {
 }
 
 fn main() {
+    // setup, args gathering
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
 
+    // process arguments
     let mut opts = Options::new();
-    opts.optopt("o", "", "set output file name", "NAME");
+    opts.optopt("o", "output", "set output file name", "FILE");
     opts.optflag("h", "help", "print this help menu");
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m }
         Err(f) => { panic!(f.to_string()) }
     };
+
+    // --help
     if matches.opt_present("h") {
         print_usage(&program, opts);
         return;
     }
-    let output = matches.opt_str("o");
+
+    // let output = matches.opt_str("o");
     let input = if !matches.free.is_empty() {
         matches.free[0].clone()
     } else {
         print_usage(&program, opts);
         return;
     };
+
+    let js_path = match fs::canonicalize(input) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
+
+    let c_js_path = CString::new(js_path.to_str().unwrap()).unwrap();
+
+    let context: *mut duk_context;
+    unsafe {
+        context = rust_duk_create_heap_default();
+        rust_duk_peval_file(context, c_js_path.as_ptr());
+        duk_destroy_heap(context);
+    }
 }
